ab59753a1ac8ce37fa36aa61d4bf47baabbcef42
user <>
2020-08-13 00:32:55 +0200

    Replace quasar inputs with native ones

--- src/components/RootView.vue
+++ src/components/RootView.vue
@@ -2,60 +2,63 @@
   <div style="padding: 15px">
       <div class="row">
         <div class="col">
-          <q-input v-model="filter.name" label="Spielname" />
+          <label>Spielname
+            <input type="text" v-model="filter.name" />
+          </label>
         </div>
       </div>
     <div class="row">
         <div class="col">
-<!--          <div class="desktop-only">-->
           <div>
-            <label for="spieldauer-range">Spieldauer</label>
-<!--            @change="val => { filter.dauer.slider = val }"-->
-            <q-range
+            <label>Spieldauer
+            <input
+                type="range"
                      id="spieldauer-range"
                      v-model="filter.dauer.slider"
                      :min="filter.dauer.min"
                      :max="filter.dauer.max"
                      :step="filter.dauer.step"
-                     lazy
-                     label-always
-                     markers
                      :left-label-value="filter.dauer.slider.min + ' Min'"
                      :right-label-value="filter.dauer.slider.max + ' Min'"
                      style="size: 90vh"
             />
+            </label>
           </div>
-<!--          <div class="desktop-hide">-->
           <div class="row">
             <div class="col-sm-12 col-md-6">
-              <q-input type="number" :max="filter.dauer.slider.max" :min="filter.dauer.min" step="15" v-model="filter.dauer.slider.min" label="min"/>
+              <label> Spieldauer min
+              <input type="number" :max="filter.dauer.slider.max" :min="filter.dauer.min" step="15" v-model="filter.dauer.slider.min"/>
+              </label>
             </div>
             <div class="col-sm-12 col-md-6">
-              <q-input type="number" :min="filter.dauer.slider.min" :max="filter.dauer.max" step="15" v-model="filter.dauer.slider.max" label="max"/>
+              <label> Spieldauer max
+              <input type="number" :min="filter.dauer.slider.min" :max="filter.dauer.max" step="15" v-model="filter.dauer.slider.max"/>
+              </label>
             </div>
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col">
-          <q-range
+          <label> Spielerzahl
+            <input
+              type="range"
             v-model="filter.spieler"
             :min="1"
             :max="12"
-            label
-            markers
             :right-label-value="filter.spieler.max < 12 ? filter.spieler.max : filter.spieler.max + '+' "
 
             style="size: 90vh"
           />
+          </label>
         </div>
     </div>
-    <input type="radio" value="abc"> abc <br>
-  <input type="checkbox">checky <br>
-  <input type="text">
+    <label>Random radio
+      <input type="radio" value="abc">
+    </label>
     <div class="row q-col-gutter-md">
       <div class="col-md-3" v-for="(spiel, index) in spiele" :key="index" :id="index">
-        <q-card class="my-card" @click="navigate(index)">
+        <div class="my-card" @click="navigate(index)">
           <img
             :src="'/cover/'+spiel.name"
             alt="this is an image!"
@@ -65,27 +68,21 @@
             <div class="absolute-bottom text-h6">
               {{spiel.name}}
             </div>
-            <template v-slot:error>
-              <div class="absolute-full flex flex-center bg-negative text-white">
-                Cannot load image
-              </div>
-            </template>
-          <q-card-section>
+          <div>
             Spieler: {{ spiel.spielerAnzahl }} Spieler <br>
             Dauer: {{spiel.dauer}} <br>
             Alter: {{spiel.mindestAlter}}
-          </q-card-section>
-        </q-card>
+          </div>
+        </div>
 
       </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <script>
 import spiele from '../assets/out.json'
-import { routeNames } from '../router'
+import { routeNames } from '@/router'
 
 export default {
   // name: 'ComponentName',
