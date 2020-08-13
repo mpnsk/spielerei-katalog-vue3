20223983cb1990f7184310cd1af0b5acee900ce3
user <>
2020-08-13 10:17:28 +0200

    Remove unneeded html

--- src/components/SpielOverview.vue
+++ src/components/SpielOverview.vue
@@ -1,80 +1,57 @@
 <template>
   <div style="padding: 15px">
-      <div class="row">
-        <div class="col">
-          <label>Spielname
-            <input type="text" v-model="filter.name" />
-          </label>
-        </div>
-      </div>
-    <div class="row">
-        <div class="col">
-          <div>
-            <label>Spieldauer
-            <input
-                type="range"
-                     id="spieldauer-range"
-                     v-model="filter.dauer.slider"
-                     :min="filter.dauer.min"
-                     :max="filter.dauer.max"
-                     :step="filter.dauer.step"
-                     :left-label-value="filter.dauer.slider.min + ' Min'"
-                     :right-label-value="filter.dauer.slider.max + ' Min'"
-                     style="size: 90vh"
-            />
-            </label>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
-              <label> Spieldauer min
-              <input type="number" :max="filter.dauer.slider.max" :min="filter.dauer.min" step="15" v-model="filter.dauer.slider.min"/>
-              </label>
-            </div>
-            <div class="col-sm-12 col-md-6">
-              <label> Spieldauer max
-              <input type="number" :min="filter.dauer.slider.min" :max="filter.dauer.max" step="15" v-model="filter.dauer.slider.max"/>
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <label> Spielerzahl
-            <input
-              type="range"
-            v-model="filter.spieler"
-            :min="1"
-            :max="12"
-            :right-label-value="filter.spieler.max < 12 ? filter.spieler.max : filter.spieler.max + '+' "
+    <label>Spielname
+      <input type="text" v-model="filter.name"/>
+    </label>
+    <label>Spieldauer
+      <input
+          type="range"
+          id="spieldauer-range"
+          v-model="filter.dauer.slider"
+          :min="filter.dauer.min"
+          :max="filter.dauer.max"
+          :step="filter.dauer.step"
+          :left-label-value="filter.dauer.slider.min + ' Min'"
+          :right-label-value="filter.dauer.slider.max + ' Min'"
+          style="size: 90vh"
+      />
+    </label>
+    <label> Spieldauer min
+      <input type="number" :max="filter.dauer.slider.max" :min="filter.dauer.min" step="15"
+             v-model="filter.dauer.slider.min"/>
+    </label>
+    <label> Spieldauer max
+      <input type="number" :min="filter.dauer.slider.min" :max="filter.dauer.max" step="15"
+             v-model="filter.dauer.slider.max"/>
+    </label>
+    <label> Spielerzahl
+      <input
+          type="range"
+          v-model="filter.spieler"
+          :min="1"
+          :max="12"
+          :right-label-value="filter.spieler.max < 12 ? filter.spieler.max : filter.spieler.max + '+' "
 
-            style="size: 90vh"
-          />
-          </label>
-        </div>
-    </div>
+          style="size: 90vh"
+      />
+    </label>
     <label>Random radio
       <input type="radio" value="abc">
     </label>
-    <div class="row q-col-gutter-md">
-      <div class="col-md-3" v-for="(spiel, index) in spiele" :key="index" :id="index">
-        <div class="my-card" @click="navigate(index)">
-          <img
-            :src="'/cover/'+spiel.name"
-            alt="this is an image!"
-            loading="lazy"
-            style="height: 150px"
-          />
-            <div class="absolute-bottom text-h6">
-              {{spiel.name}}
-            </div>
-          <div>
-            Spieler: {{ spiel.spielerAnzahl }} Spieler <br>
-            Dauer: {{spiel.dauer}} <br>
-            Alter: {{spiel.mindestAlter}}
-          </div>
-        </div>
-
+    <div v-for="(spiel, index) in spiele" :key="index" :id="index" class="my-card" @click="navigate(index)">
+      <img
+          :src="'/cover/'+spiel.name"
+          alt="this is an image!"
+          loading="lazy"
+          style="height: 150px"
+      />
+      <div class="absolute-bottom text-h6">
+        {{ spiel.name }}
+      </div>
+      <div>
+        Spieler: {{ spiel.spielerAnzahl }} Spieler <br>
+        Dauer: {{ spiel.dauer }} <br>
+        Alter: {{ spiel.mindestAlter }}
       </div>
     </div>
   </div>
@@ -82,11 +59,11 @@
 
 <script>
 import spiele from '../assets/spiele.json'
-import { routeNames } from '@/router'
+import {routeNames} from '@/router'
 
 export default {
   // name: 'ComponentName',
-  data () {
+  data() {
     return {
       routeNames: routeNames,
       deferList: false,
@@ -111,23 +88,24 @@ export default {
     }
   },
   methods: {
-    navigate (index) {
+    navigate(index) {
       this.$router.push({
         name: this.routeNames.dialog,
-        params: { userId: index }
+        params: {userId: index}
       })
     }
   },
 }
 </script>
 <style scoped>
-  .my-card {
-      height: 100%;
-      max-width: 500px;
-  }
+.my-card {
+  height: 100%;
+  max-width: 500px;
+  padding: 15px;
+}
 
-    .my-card:hover {
-        box-shadow: 1px 8px 20px grey;
-        -webkit-transition: box-shadow .6s ease-in-out;
-    }
+.my-card:hover {
+  box-shadow: 1px 8px 20px grey;
+  -webkit-transition: box-shadow .6s ease-in-out;
+}
 </style>
