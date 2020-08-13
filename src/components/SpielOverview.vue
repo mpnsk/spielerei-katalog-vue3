7fc38031c012dc72bea0721e3302a364b9d7c416
user <>
2020-08-13 10:19:26 +0200

    Remove old vue data

--- src/components/SpielOverview.vue
+++ src/components/SpielOverview.vue
@@ -1,43 +1,43 @@
 <template>
   <div style="padding: 15px">
-    <label>Spielname
-      <input type="text" v-model="filter.name"/>
-    </label>
-    <label>Spieldauer
-      <input
-          type="range"
-          id="spieldauer-range"
-          v-model="filter.dauer.slider"
-          :min="filter.dauer.min"
-          :max="filter.dauer.max"
-          :step="filter.dauer.step"
-          :left-label-value="filter.dauer.slider.min + ' Min'"
-          :right-label-value="filter.dauer.slider.max + ' Min'"
-          style="size: 90vh"
+    <div>
+      <label>Titel
+        <input type="text" v-model="filter.name"/>
+      </label>
+    </div>
+    <div>
+      <label>Dauer
+        <input
+            type="range"
+        />
+      </label>
+    </div>
+    <div>
+    <label> Spieldauer min
+      <input type="number"
       />
     </label>
-    <label> Spieldauer min
-      <input type="number" :max="filter.dauer.slider.max" :min="filter.dauer.min" step="15"
-             v-model="filter.dauer.slider.min"/>
-    </label>
+    </div>
+    <div>
     <label> Spieldauer max
-      <input type="number" :min="filter.dauer.slider.min" :max="filter.dauer.max" step="15"
-             v-model="filter.dauer.slider.max"/>
+      <input type="number"
+      />
     </label>
+    </div>
+    <div>
     <label> Spielerzahl
       <input
           type="range"
-          v-model="filter.spieler"
           :min="1"
           :max="12"
-          :right-label-value="filter.spieler.max < 12 ? filter.spieler.max : filter.spieler.max + '+' "
-
-          style="size: 90vh"
       />
     </label>
+    </div>
+    <div>
     <label>Random radio
       <input type="radio" value="abc">
     </label>
+    </div>
     <div v-for="(spiel, index) in spiele" :key="index" :id="index" class="my-card" @click="navigate(index)">
       <img
           :src="'/cover/'+spiel.name"
@@ -62,29 +62,11 @@ import spiele from '../assets/spiele.json'
 import {routeNames} from '@/router'
 
 export default {
-  // name: 'ComponentName',
   data() {
     return {
       routeNames: routeNames,
-      deferList: false,
       spiele,
-      filter: {
-        spieler: {
-          min: 1,
-          max: 12,
-          lower: 2,
-          upper: 4
-        },
-        dauer: {
-          slider: {
-            min: 15,
-            max: 120
-          },
-          min: 15,
-          max: 120,
-          step: 15
-        }
-      }
+      filter: {}
     }
   },
   methods: {
