defd029452222e769678ffd3ba31c81eb4e28e73
user <>
2020-08-14 22:39:54 +0200

    Add empty ToggleDiv component and toggle-div functionality

--- src/components/SpielOverview.vue
+++ src/components/SpielOverview.vue
@@ -1,6 +1,31 @@
 <template>
   <div style="padding: 15px">
     <div>
+      <div style="padding: 15px;">
+        <div>
+          <span v-bind:class="{
+          activeTag: filter.div1,
+          inactiveTag: !filter.div1
+         }"
+                @click="filter.div1 = !filter.div1">
+          unter 15 Min
+        </span>
+          <span v-bind:class="{ activeTag: filter.div2, inactiveTag: !filter.div2 }"
+                @click="filter.div2 = !filter.div2">
+          15-30 Min
+        </span>
+          <span v-bind:class="{ activeTag: filter.div3, inactiveTag: !filter.div3 }"
+                @click="filter.div3 = !filter.div3">
+          30-45 Min
+        </span>
+          <button style="">example btn</button>
+        </div>
+        <span class="activeTag" @click="filter.div1 = !filter.div1">div1</span>
+        <span class="inactiveTag">div2</span>
+        <toggle-div active-class="just a class">abc</toggle-div>
+        <toggle-div active-class="another string"></toggle-div>
+        <toggle-div></toggle-div>
+      </div>
       <label>Titel
         <input type="text" v-model="filter.name"/>
       </label>
@@ -13,30 +38,30 @@
       </label>
     </div>
     <div>
-    <label> Spieldauer min
-      <input type="number"
-      />
-    </label>
+      <label> Spieldauer min
+        <input type="number"
+        />
+      </label>
     </div>
     <div>
-    <label> Spieldauer max
-      <input type="number"
-      />
-    </label>
+      <label> Spieldauer max
+        <input type="number"
+        />
+      </label>
     </div>
     <div>
-    <label> Spielerzahl
-      <input
-          type="range"
-          :min="1"
-          :max="12"
-      />
-    </label>
+      <label> Spielerzahl
+        <input
+            type="range"
+            :min="1"
+            :max="12"
+        />
+      </label>
     </div>
     <div>
-    <label>Random radio
-      <input type="radio" value="abc">
-    </label>
+      <label>Random radio
+        <input type="radio" value="abc">
+      </label>
     </div>
     <div v-for="(spiel, index) in spiele" :key="index" :id="index" class="my-card" @click="navigate(index)">
       <img
@@ -60,13 +85,18 @@
 <script>
 import spiele from '../assets/spiele.json'
 import {routeNames} from '@/router'
+import ToggleDiv from "@/components/ToggleDiv";
 
 export default {
   data() {
     return {
       routeNames: routeNames,
       spiele,
-      filter: {}
+      filter: {
+        div1: true,
+        div2: true,
+        div3: true
+      }
     }
   },
   methods: {
@@ -77,6 +107,9 @@ export default {
       })
     }
   },
+  components: {
+    ToggleDiv
+  }
 }
 </script>
 <style scoped>
@@ -89,5 +122,23 @@ export default {
 .my-card:hover {
   box-shadow: 1px 8px 20px grey;
   -webkit-transition: box-shadow .6s ease-in-out;
+}
+
+.inactiveTag {
+  color: orange;
+  border: 1px solid;
+  border-radius: 5px;
+  padding: 2px;
+}
+.inactiveTag:hover {
+  color: grey;
+}
+
+.activeTag {
+  color: white;
+  background-color: orange;
+  border: 1px solid;
+  border-radius: 5px;
+  padding: 2px;
 }
 </style>
---
+++ src/components/ToggleDiv.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>slot says:
+    <slot>default</slot>
+    , {{ activeClass }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToggleDiv",
+  data() {
+    return {}
+  },
+  props: {
+    activeClass: {
+      type: String,
+      default: 'default prop'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
