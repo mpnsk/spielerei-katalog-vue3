7a2cd55350d1bf49fb4a5292760bc7db370790b2
user <>
2020-08-16 22:47:34 +0200

    Import tailwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import tailwindbase from 'tailwindcss/dist/base.css'
+import tailwindcomponents from 'tailwindcss/dist/components.css'
+import tailwind from 'tailwindcss/dist/tailwind.css'
+import tailwindutilities from 'tailwindcss/dist/utilities.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(tailwindbase)
+    .use(tailwindcomponents)
+    .use(tailwind)
+    .use(tailwindutilities)
+    .mount('#app')
