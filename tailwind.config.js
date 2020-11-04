ada970e066a92030c478ec23fddf13506e98aaa3
user <>
2020-11-04 11:23:46 +0100

    Add kinda working postcss config

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    purge: {
+        enabled: true,
+        // mode: 'all',
+        content: ['./index.html', './src/**/*.vue', './src/**/*.js'],
+    },
+    // theme: {
+    //   extend: {
+    //     // here's how to extend fonts if needed
+    //     fontFamily: {
+    //       sans: [...defaultTheme.fontFamily.sans],
+    //     },
+    //   },
+    // },
+    plugins: [
+        // require('@tailwindcss/ui')({
+        //   layout: 'sidebar',
+        // }),
+        require('@tailwindcss/typography'),
+    ],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+}
