d203210a755f48bc395d74f8015cea638b5def15
user <>
2020-11-04 11:20:07 +0100

    Update url to work on phone

--- src/GameStore.js
+++ src/GameStore.js
@@ -1,7 +1,7 @@
 import {reactive} from "@vue/reactivity";
 
 let store = reactive([]);
-fetch(new Request('http://localhost:8080/games'))
+fetch(new Request('http://192.168.178.147:8080/games'))
     .then(response => response.json())
     .then(json => json.forEach(x => store.push(x)));
 export default store
