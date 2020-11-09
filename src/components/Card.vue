edf25edeb434f5804c6550948283b27c1cc8a171
user <>
2020-11-09 20:38:31 +0100

    Update for lazysizes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
     <div class="container flex-1">
 
       <img
-          v-bind:src="spiel.coverUrl"
+          v-bind:data-src="spiel.coverUrl"
           alt="this is an image!"
           class="lazyload place-self-center object-center "
       />
